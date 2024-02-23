c9e8958114e4a22ed69e0e44ef66e24102c73a6c
Bernardo Narvaez <erisboxx@gmail.com>
2024-02-23 16:22:06 -0600

    change jwt cookie domain to production server

--- authentication-service/handler/cookie.go
+++ authentication-service/handler/cookie.go
@@ -10,7 +10,7 @@ func CreateJWTCookie(jwtToken string) *http.Cookie {
 		Name:     "jwt",
 		Value:    jwtToken,
 		Path:     "/",
-		Domain:   "localhost",
+		Domain:   "bernardn.com",
 		Expires:  time.Now().Add(time.Minute * 30),
 		MaxAge:   3600,
 		HttpOnly: true,
